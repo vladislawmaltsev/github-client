3611a438ce5a61baf2519fb9526d038fba3d261e
vladislawmaltsev <dried.dill@gmail.com>
2019-12-04 14:01:50 +0300

    add new features from last repo

--- src/App/index.js
+++ src/App/index.js
@@ -10,51 +10,77 @@ import * as routes from '../constants/routes';
 
 import './style.css';
 
+const CLIENT_ID = "a41eb2893d39b444824a";
+const REDIRECT_URI = "http://localhost:3000/profile";
+
 class App extends Component {
-  state = {
-    organizationName: 'facebook',
-  };
+    state = {
+        organizationName: 'facebook',
+        token: null
+    };
 
-  onOrganizationSearch = value => {
-    this.setState({ organizationName: value });
-  };
+    componentDidMount() {
+        const code =
+            window.location.href.match(/\?code=(.*)/) &&
+            window.location.href.match(/\?code=(.*)/)[1];
+        if (code) {
+            fetch(`https://github-react-client.herokuapp.com/authenticate/${code}`)
+                .then(response => response.json())
+                .then(({ token }) => {
+                    this.setState({
+                        token,
+                    });
+                });
+        }
+    }
 
-  render() {
-    const { organizationName } = this.state;
+    onOrganizationSearch = value => {
+        this.setState({ organizationName: value });
+    };
 
-    return (
-      <Router>
-        <div className="App">
-          <Navigation
-            organizationName={organizationName}
-            onOrganizationSearch={this.onOrganizationSearch}
-          />
+    render() {
+        const { organizationName } = this.state;
+        return (
+            <Router>
 
-          <div className="App-main">
-            <Route
-              exact
-              path={routes.ORGANIZATION}
-              component={() => (
-                <div className="App-content_large-header">
-                  <Organization organizationName={organizationName} />
+                <div className="App">
+                    <Navigation
+                        organizationName={organizationName}
+                        onOrganizationSearch={this.onOrganizationSearch}
+                    />
+
+                    <div className="App-main">
+                        <Route
+                            exact
+                            path={routes.ORGANIZATION}
+                            component={() => (
+                                <div className="App-content_large-header">
+                                    <Organization organizationName={organizationName} />
+                                </div>
+                            )}
+                        />
+                        <Route
+                            exact
+                            path={routes.PROFILE}
+                            component={() => (
+                                <div className="App-content_small-header">
+                                    <Profile />
+                                </div>
+                            )}
+                        />
+                    </div>
+                    <div>
+                        <a
+                            href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+                        >
+                            Login
+                        </a>
+                    </div>
+                    <Footer />
                 </div>
-              )}
-            />
-            <Route
-              exact
-              path={routes.PROFILE}
-              component={() => (
-                <div className="App-content_small-header">
-                  <Profile />
-                </div>
-              )}
-            />
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+            </Router>
+        );
+    }
 }
 
 export default App;
